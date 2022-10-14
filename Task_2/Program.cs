99d860c8552c5bfddb631d5f9f8bcdae1cd1a404
Darya <Darsy_13@bk.ru>
2022-10-14 15:39:22 +0300

    Указали условия задачи 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Input a first number: ");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input a first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Input a second number: ");
