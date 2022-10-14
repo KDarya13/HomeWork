505589e9210f6b44cb4e038fcdf4a945d4ec4468
Darya <Darsy_13@bk.ru>
2022-10-14 15:25:42 +0300

    Решение задачи 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(a < b)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+if(max > c)
+{
+    Console.WriteLine("max = " + max);
+}
+else
+{
+    Console.WriteLine("max = " + c);
+}
