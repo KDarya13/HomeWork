c003392713a09ba3ebc8272da26605bd8320c63e
Darya <Darsy_13@bk.ru>
2022-10-14 15:38:06 +0300

    Указали условия задачи

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Input a first number: ");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input a first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Input a second number: ");
