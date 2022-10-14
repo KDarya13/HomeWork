85f707db5ee7fb490012ab154c71484f04088a44
Darya <Darsy_13@bk.ru>
2022-10-14 15:22:31 +0300

    Решение задачи 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine($"max = {a} min = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {b} min = {a}");
+}
