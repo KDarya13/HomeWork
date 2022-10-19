d30387f155dc677203a8076358f7b7d76f573724
Darya <Darsy_13@bk.ru>
2022-10-19 08:38:49 +0300

    Решение задачи 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+int SecondNumber(int number)
+{
+    int num = (number / 10) % 10;
+    return num;
+}
+
+Console.WriteLine("Input a three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = SecondNumber(n);
+
+Console.WriteLine($"The second number is: {result}");
