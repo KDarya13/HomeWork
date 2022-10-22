a4a01e8c2082f62be0da87b9042a6fc3f49d89ab
Darya <Darsy_13@bk.ru>
2022-10-22 15:39:03 +0300

    Решение задачи 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int number)
+{
+    int cur = 1;
+    if(number > 0)
+    {
+        while(cur <= number)
+        {   
+            Console.Write(cur * cur * cur + " ");
+            cur++;
+        } 
+    }
+    else
+    {
+        Console.Write("This number is negative");
+    }   
+
+}
+
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Cube(a);
