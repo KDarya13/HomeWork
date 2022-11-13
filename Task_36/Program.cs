74af8f214a54672ebe225c57ae82bda7e84d88c7
Darya <Darsy_13@bk.ru>
2022-11-14 00:07:05 +0300

    Решение задачи 36

---
+++ Task_36/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length;i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+} 
+
+int FindSumOddNumber(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i+=2)
+        sum = sum + array[i];
+    return sum;    
+}
+
+Console.Write("Input a number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(n, min, max);
+ShowArray(myArray);
+
+int sumoddnumber = FindSumOddNumber(myArray);
+Console.WriteLine($"Sum of numbers on odd positions is {sumoddnumber}");
+
+
+
+    
